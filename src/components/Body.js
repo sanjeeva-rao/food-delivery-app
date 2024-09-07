15d77a5db6e7f9ddaa2785c45c01020d6002501c
sanjeeva-rao <sanjeevarao436@gmail.com>
2024-09-07 19:09:03 +0530

    added search restaurant option

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,23 +5,20 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
     const [restaurantData, setRestaurantData] = useState([]);
+    const [defaultResData, setDefaultResData] = useState([]);
+    const [searchResName, setSearchResName] = useState("");
 
     var fetchData = async () => {
         const data = await fetch("https://api.allorigins.win/raw?url=" + encodeURIComponent("https://www.swiggy.com/dapi/restaurants/list/v5?lat=11.01420&lng=76.99410&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"));
         const json = await data.json();
         var restaurentList = json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
-        setRestaurantData(restaurentList)
+        setRestaurantData(restaurentList);
+        setDefaultResData(restaurentList);
     }
     
     useEffect(()=>{
         fetchData()  
     }, [])
-    const filterTopRests = () => {
-        var topRes = restaurantData.filter((res)=>{
-            return res.info.avgRating > 4.5
-        })
-        setRestaurantData(topRes)
-    }
 
     if(!restaurantData.length){
         return <Shimmer/>
@@ -30,7 +27,17 @@ const Body = () => {
     return(
         (
             <div className="body">
-                <button onClick={()=>filterTopRests()}>Top Restaurents</button>
+                <div className="search-bar">
+                    <input type="text" value={searchResName} placeholder="Search Reastaurant Name" onChange={(e)=>{
+                        setSearchResName(e.target.value);  
+                    }}/>
+                    <button className="search-button" onClick = {()=>{
+                        const filterRes = defaultResData.filter((res)=>{
+                            return res.info.name.toUpperCase().includes(searchResName.toUpperCase());
+                        })
+                        setRestaurantData(filterRes);
+                    }}>Search</button>
+                </div>
                 <div className="res-card-body">
                 {
                         restaurantData.map((res)=>{
